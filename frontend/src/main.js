36f91d660eed13514fd1403a6e749002f74acf88
iso53 <102249575+ISO53@users.noreply.github.com>
2026-08-19 19:48:43 +0300

    didn't like that library. Im gonna use shadcn-vue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
-
-import "./assets/base.css";
+import "./style.css";
 
 const app = createApp(App);
 
