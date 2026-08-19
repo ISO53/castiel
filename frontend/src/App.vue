5cc93665816089398d2e9ea036e6d1619e43d083
iso53 <102249575+ISO53@users.noreply.github.com>
2026-08-19 22:17:20 +0300

    added the resizeable panels

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,39 @@
-<script setup></script>
+<template>
+	<div class="app">
+		<TopBar />
+		<ResizablePanelGroup direction="horizontal">
+			<ResizablePanel collapsible :defaultSize="20" :minSize="3" :maxSize="45"></ResizablePanel>
 
-<template></template>
+			<ResizableHandle />
 
-<style scoped></style>
+			<ResizablePanel>
+				<ResizablePanelGroup direction="vertical">
+					<ResizablePanel :minSize="40"></ResizablePanel>
+
+					<ResizableHandle />
+
+					<ResizablePanel collapsible :defaultSize="40" :minSize="8"></ResizablePanel>
+				</ResizablePanelGroup>
+			</ResizablePanel>
+
+			<ResizableHandle />
+
+			<ResizablePanel collapsible :defaultSize="25" :minSize="3" :maxSize="45">
+				<ChatView />
+			</ResizablePanel>
+		</ResizablePanelGroup>
+	</div>
+</template>
+
+<script setup>
+import { ResizablePanelGroup, ResizablePanel, ResizableHandle } from "@/components/ui/resizable";
+import ChatView from "@/views/ChatView.vue";
+import TopBar from "@/views/TopBar.vue";
+</script>
+
+<style scoped>
+.app {
+	width: 100vw;
+	height: 100vh;
+}
+</style>
---
+++ frontend/src/views/TopBar.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+
+	</div>
+</template>
+
+<script>
+export default {
+	name: "TopBar",
+	data() {
+		return {
+
+		};
+	},
+}
+</script>
+
+<style scoped>
+
+</style>
